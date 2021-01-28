b97c8fd7dd6548cc2286f814d544b07c8442cd88
sebastiancatano <77751217+sebastiancatano@users.noreply.github.com>
2021-01-28 16:49:14 -0500

    Add files via upload

---
+++ Tutoría2(IMC).cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Tutoría_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Entradas
+            Console.Write("Ingrese su masa corporal en kilogramos: ");
+            double m = double.Parse(Console.ReadLine());
+            Console.Write("Ingrese su estatura en metros: ");
+            double a = double.Parse(Console.ReadLine());
+
+            //Proceso
+
+            double IMC = m / (a * a);
+
+            if (IMC < 18.5)
+            {
+                Console.WriteLine("Su IMC es inferior al normal");
+            }
+            else if (18.5 <= IMC && IMC <= 24.9)
+            {
+                Console.WriteLine("Su IMC está dentro del promedio");
+            }
+            else if (25 <= IMC && IMC <= 29.9)
+            {
+                Console.WriteLine("Su IMC está sobre el promedio");
+            }
+            else if (IMC > 30)
+            {
+                Console.WriteLine("Su IMC está dentro de la categoría de obesidad");
+            }
+
+
+
+
+
+
+        }
+    }
+}
