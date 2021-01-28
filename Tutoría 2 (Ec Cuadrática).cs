e7631786b711e48064df6f811e7166b91183c1e4
sebastiancatano <77751217+sebastiancatano@users.noreply.github.com>
2021-01-28 17:07:15 -0500

    Add files via upload

---
+++ Tutoría 2 (Ec Cuadrática).cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Tutoría_2._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.Write("Ingrese el dato a: ");
+            double a = double.Parse(Console.ReadLine());
+            Console.Write("Ingrese el dato b: ");
+            double b = double.Parse(Console.ReadLine());
+            Console.Write("Ingrese el dato c: ");
+            double c = double.Parse(Console.ReadLine());
+
+            //Proceso
+            double x1 = (-b + Math.Sqrt(b * b - 4 * a * c)) / (2 * a);
+            double x2 = (-b - Math.Sqrt(b * b - 4 * a * c)) / (2 * a);
+
+            double d = b * b - 4 * a * c;
+            if (d > 0)
+            {
+                Console.Write("La ecuación tiene 2 posibles soluciones: ");
+                Console.Write("X1: " + x1);
+                Console.Write("X2: " + x2);
+            }
+            else if (d == 0)
+            {
+                Console.Write("X1: " + x1);
+            }
+            else if (d < 0)
+            {
+                Console.Write("La ecuación no tiene una solución ");
+            }
+        }
+    }
+}
