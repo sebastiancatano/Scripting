9b47d0fded28769ed9110e0957aedbeb04f2c3fb
sebastiancatano <77751217+sebastiancatano@users.noreply.github.com>
2021-02-22 17:11:01 -0500

    Add files via upload

---
+++ Euler.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Euler
+{
+    class Program
+    {
+        //Video https://drive.google.com/file/d/1bGCgusZkAKlwnvt_SCvQM18OjeLQL1fn/view?usp=sharing
+        static void Main(string[] args)
+        {
+            double x = 3, n = 100, e = 0;
+            for (int i = 0; i <= n; i++)
+            {
+                e += (Math.Pow(x,i))/Factorial(i);        
+            }
+            Console.WriteLine(e);
+        }
+        static double Factorial (double valor)
+        {
+            double total = 1;
+            for (int i = 1; i<= valor; i++)
+            {
+                total *= i;
+            }
+            return total;
+        }
+    }
+}
---
+++ Sin(x).cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ssin_x_
+{
+    class Program
+    {
+        //Video https://drive.google.com/file/d/1bGCgusZkAKlwnvt_SCvQM18OjeLQL1fn/view?usp=sharing
+        static void Main(string[] args)
+        {
+            double x = 0.5, n = 100, e = 0;            
+            for (int i = 0; i <= n; i++)
+            {
+                e += ((Math.Pow(-1, i)) / Factorial(2 * i + 1)) * Math.Pow(x, 2 * i + 1);
+            }
+            Console.WriteLine(e);
+        }
+        static double Factorial(double valor)
+        {
+            double total = 1;
+            for (int i = 1; i <= valor; i++)
+            {
+                total *= i;
+            }
+            return total;
+        }
+    }
+}
