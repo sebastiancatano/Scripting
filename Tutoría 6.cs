9a9008130ed710070f97a344473a4b83228fe544
sebastiancatano <77751217+sebastiancatano@users.noreply.github.com>
2021-02-28 15:41:29 -0500

    Add files via upload

---
+++ Tutoría 6.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Tutoría_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+                string[] nombres = { "Nezuko", "Tanjiro", "Zenitsu", "Inosuke", "Genya", "Kanao", "Tomioka" };
+                double[] estatura = { 153, 165, 164.5, 164, 180, 156, 176 };
+                double[] distanciaPro = new double[estatura.Length];
+                double total = 0, min = 200;
+                int indice = 0;
+                for (int i = 0; i < estatura.Length; i++)
+                {
+                    total += estatura[i];                  
+                }             
+                double promedio = total / estatura.Length;
+                Console.WriteLine("PROMEDIO " + promedio);                                            
+                for (int i = 0; i < estatura.Length - 1; i++)
+                {                 
+                    distanciaPro[i] = Math.Abs(promedio - estatura[i]);
+                    if (distanciaPro[i] < min)
+                    {
+                        min = distanciaPro[i];
+                        indice =i;
+                    }
+                }
+                Console.WriteLine("Distancia menor " + estatura[indice] + " "+ nombres[indice]);
+            }
+        }
+    }
+}
